529a3a3a39a0ee6ca6abce6e6ef250d64385cd94
Thunder19 <braydenhamous@gmail.com>
2021-01-30 23:14:55 -0600

    2

--- Rejecting Death/Assets/Scripts/Player/Movement.cs
+++ Rejecting Death/Assets/Scripts/Player/Movement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField]
+    float moveSpeed = 1;
+
+    [SerializeField]
+    float jumpSpeed = 1;
+
+    public bool isGrounded;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+
+        bool player_jump = Input.GetButtonDown("Jump");
+
+        if (player_jump && isGrounded)
+        {
+            
+        }
     }
 }
