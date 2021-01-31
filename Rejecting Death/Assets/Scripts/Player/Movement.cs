62b39c1f7f1dd0f3f3bf1cd6492c3a178c0ce0c1
Thunder19 <braydenhamous@gmail.com>
2021-01-31 17:08:23 -0600

    3

--- Rejecting Death/Assets/Scripts/Player/Movement.cs
+++ Rejecting Death/Assets/Scripts/Player/Movement.cs
@@ -2,27 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
     [SerializeField]
-    float moveSpeed = 1;
+    public float moveSpeed = 1;
 
     [SerializeField]
     float jumpSpeed = 1;
 
     public bool isGrounded;
 
-    Rigidbody2D rb;
+    private Rigidbody2D playerRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
+        if(playerRigidbody != null)
+        {
+            ApplyInput();
+        }
+        else
+        {
+            Debug.LogWarning("Rigid body not attached to player " + gameObject.name);
+        }
         float h = Input.GetAxis("Horizontal");
 
         bool player_jump = Input.GetButtonDown("Jump");
@@ -31,5 +40,20 @@ public class Movement : MonoBehaviour
         {
             
         }
+    }
+
+    public void ApplyInput()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        float xForce = xInput * moveSpeed * Time.deltaTime;
+
+        Vector2 force = new Vector2(xForce, 0);
+
+        playerRigidbody.AddForce(force);
+
+        Debug.Log(xForce);
+        Debug.Log(playerRigidbody.velocity);
     }
 }
