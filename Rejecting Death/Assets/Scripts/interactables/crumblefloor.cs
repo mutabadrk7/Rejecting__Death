ecaa39be367f9607d7b6ccd4267f1df8c8c17682
mutabadrk7 <54857665+mutabadrk7@users.noreply.github.com>
2021-02-03 12:27:43 -0800

    update

---
+++ Rejecting Death/Assets/Scripts/interactables/crumblefloor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crumblefloor : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    public float fallDelay;
+    public float respawnDelay;
+    private Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        startPos = new Vector2(transform.position.x, transform.position.y);
+        GetComponent<EdgeCollider2D>().isTrigger = false;
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        //if(GetComponent<EdgeCollider2D>().isTrigger = true)
+           
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.collider.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }    
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rb2d.isKinematic = false;
+        GetComponent<EdgeCollider2D>().isTrigger = true;
+        
+        yield return 0;
+    }
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnDelay);
+        StartCoroutine(Respawn());
+        rb2d.isKinematic = false;
+        transform.position = startPos;
+        
+        GetComponent<EdgeCollider2D>().isTrigger = false;
+        yield return 0;
+    }
+}
