1a7256b5c40b02ed35bff4d5f8a03dcb6bedb53e
mutabadrk7 <54857665+mutabadrk7@users.noreply.github.com>
2021-02-04 22:03:47 -0800

    update
    the key mechanic is not  done im trying to figure out how to make it follow the player then make it move to the door

---
+++ Rejecting Death/Assets/Scripts/interactables/key.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : MonoBehaviour
+{
+
+    private bool isFollowing;
+
+    public float followSpeed;
+
+    public Transform folowTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFollowing)
+        {
+            transform.position = Vector3.Lerp(transform.position, folowTarget.position, followSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (CompareTag("Player"))
+        {
+            if (isFollowing)
+            {
+                CharacterController thePlayer = FindObjectOfType<CharacterController>();
+
+                folowTarget = thePlayer.transform;
+            }
+        }
+    }
+}
