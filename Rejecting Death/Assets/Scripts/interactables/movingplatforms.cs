80670b4f7d7f22b1d8047a02ab2aaf17553b81eb
mutabadrk7 <54857665+mutabadrk7@users.noreply.github.com>
2021-02-04 16:07:47 -0800

    updated falling platform and moving pladform script

---
+++ Rejecting Death/Assets/Scripts/interactables/movingplatforms.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingplatforms : MonoBehaviour
+{
+    public Vector3 startSpot;
+    public Vector3 endSpot;
+
+    private bool moving;
+
+    public Vector3 moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startSpot = transform.position;        
+        endSpot = new Vector3(endSpot.x, endSpot.y,0);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            transform.position += (moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            moving = true;
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            collision.collider.transform.SetParent(null);
+            moving = false;
+        }
+    }
+
+
+}
