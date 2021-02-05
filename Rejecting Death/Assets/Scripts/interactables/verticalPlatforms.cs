5ab8eccd4dbf75084bbef30462d44688174bb0cf
mutabadrk7 <54857665+mutabadrk7@users.noreply.github.com>
2021-02-05 11:57:50 -0800

    my update
    added crubling platform and a falllthrough platform (press down arrow) and the script for the key (invomplete) and themoving platform (incomplet?)

---
+++ Rejecting Death/Assets/Scripts/interactables/verticalPlatforms.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class verticalPlatforms : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    public float waitTime;
+
+
+    private void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            waitTime = 0.5f;
+        }
+            if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if(waitTime >= 0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTime = 0.5f;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            effector.rotationalOffset = 0f;
+        }
+
+    }
+}
